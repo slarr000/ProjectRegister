9b6dcb5ef4dc902a710a01c381584647118640d6
slarr000 <yarik.gim52@gmail.com>
2025-10-03 15:49:47 +0300

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -3,12 +3,15 @@ use ProjectRegister\Controllers\AuthController;
 use ProjectRegister\Middleware\AuthMiddleware;
 use Slim\Routing\RouteCollectorProxy;
 
+// Главная страница и аутентификация
 $app->get('/', [AuthController::class, 'showHome'])->setName('home');
 $app->get('/register', [AuthController::class, 'showRegister'])->setName('register');
-$app->post('/register', [AuthController::class, 'register']);
 $app->get('/login', [AuthController::class, 'showLogin'])->setName('login');
-$app->post('/login', [AuthController::class, 'login']);
 $app->get('/logout', [AuthController::class, 'logout'])->setName('logout');
+
+// API endpoints (только POST)
+$app->post('/api/register', [AuthController::class, 'register']);
+$app->post('/api/login', [AuthController::class, 'login']);
 
 // Защищенные маршруты
 $app->group('', function (RouteCollectorProxy $group) {
